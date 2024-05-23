19bcd1a429a4efed74aa52814d8d60c97e73216f
Philip Slisenko <slisenkophilip@gmail.com>
2024-05-23 10:05:12 -0600

    x

--- src/services/periodic_tasks/tg_id_reply.py
+++ src/services/periodic_tasks/tg_id_reply.py
@@ -4,7 +4,10 @@ from src.utils.telegram import get_updates, send_message
 def periodic_tg():
     last_update_id = 0
     while True:
-        updates = get_updates(last_update_id)
+        try:
+            updates = get_updates(last_update_id)
+        except:
+            continue
         for update in updates:
             if "message" in update:
                 chat_id = update["message"]["chat"]["id"]
