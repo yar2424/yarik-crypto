c4d4598a55e85fc818046c3450d9a1c012537d38
Philip Slisenko <slisenkophilip@gmail.com>
2024-06-27 22:13:18 -0600

    r

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ cleanup() {
 # Trap SIGINT (Ctrl+C) and SIGTERM and call the cleanup function
 trap cleanup SIGINT SIGTERM
 
-export ECS_PUBLIC_IP=$(curl -s ${ECS_CONTAINER_METADATA_URI_V4}/task | jq -r '.Containers[0].Networks[0].IPv4Addresses[0]')
+export ECS_PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 echo $ECS_PUBLIC_IP
 
 # command > >(tee /proc/$$/fd/1) 2> >(tee /proc/$$/fd/2 >&2)
